581e04f4c3ee74da03e00cf0765884f89af7e4d2
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-07 03:57:05 +1000

    .

--- src/components/CamList.tsx
+++ src/components/CamList.tsx
@@ -1,10 +1,12 @@
 import "../css/cam-list.css";
+import { cams } from "../data/cams";
 
 function CamList() {
   return (
     <div className="cam-list">
-      <div>item 1</div>
-      <div>item 2</div>
+      {cams.map((beach) => {
+        return <div>item 1</div>;
+      })}
     </div>
   );
 }
---
+++ src/data/cams.ts
@@ -0,0 +1,128 @@
+interface Beach {
+  name: string;
+  url: string;
+}
+
+export const cams: Beach[] = [
+  {
+    name: "seaway",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-seaway/playlist.m3u8",
+  },
+  {
+    name: "seawayspit",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-seawayspit/playlist.m3u8",
+  },
+  {
+    name: "mainbeach",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-mainbeach/playlist.m3u8",
+  },
+  {
+    name: "narrowneck",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-narrowneck/playlist.m3u8",
+  },
+  {
+    name: "artreef",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-artreef/playlist.m3u8",
+  },
+  {
+    name: "surfersparadise",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-surfersparadise/playlist.m3u8",
+  },
+  {
+    name: "kurrawa",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-kurrawa/playlist.m3u8",
+  },
+  {
+    name: "burleighheads overview",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-burleighheadsov/playlist.m3u8",
+  },
+  {
+    name: "burleighheads",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-burleighheads/playlist.m3u8",
+  },
+  {
+    name: "palmbeachqld",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-palmbeachqld/playlist.m3u8",
+  },
+  {
+    name: "currumbinalleyoverview",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-currumbinalleyoverview/playlist.m3u8",
+  },
+  {
+    name: "tugun",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-tugun/playlist.m3u8",
+  },
+  {
+    name: "kirranorth",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-kirranorth/playlist.m3u8",
+  },
+  {
+    name: "kirrahigh",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-kirrahigh/playlist.m3u8",
+  },
+  {
+    name: "greenmounthigh",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-greenmounthigh/playlist.m3u8",
+  },
+  {
+    name: "rainbowbaynorth",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-rainbowbaynorth/playlist.m3u8",
+  },
+  {
+    name: "greenmountoverview",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-greenmountoverview/playlist.m3u8",
+  },
+  {
+    name: "snapperoverview",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-snapperoverview/playlist.m3u8",
+  },
+  {
+    name: "snapperrocksnorth",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-snapperrocksnorth/playlist.m3u8",
+  },
+  {
+    name: "duranbah",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-duranbah/playlist.m3u8",
+  },
+  {
+    name: "kingscliff",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-kingscliff/playlist.m3u8",
+  },
+  {
+    name: "cudgen",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-cudgen/playlist.m3u8",
+  },
+  {
+    name: "cabarita",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-cabarita/playlist.m3u8",
+  },
+  {
+    name: "brunswickheads",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-brunswickheads/playlist.m3u8",
+  },
+
+  {
+    name: "byronbay",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-byronbay/playlist.m3u8",
+  },
+  {
+    name: "thepassoverview",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-thepassoverview/playlist.m3u8",
+  },
+  {
+    name: "lennoxhead",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-lennoxhead/playlist.m3u8",
+  },
+  {
+    name: "ballinashellybeach",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-ballinashellybeach/playlist.m3u8",
+  },
+  {
+    name: "ballinalighthouse",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-ballinalighthouse/playlist.m3u8",
+  },
+  {
+    name: "evanshead",
+    url: "https://cams.cdn-surfline.com/cdn-au/au-evanshead/playlist.m3u8",
+  },
+];
