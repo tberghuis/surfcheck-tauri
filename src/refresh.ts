54e8afeefe468277f0574f62fdd94716858a1f19
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-07 06:37:56 +1000

    .

---
+++ src/refresh.ts
@@ -0,0 +1,7 @@
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "r") {
+    // alert('Undo!');
+    console.log("refresh");
+    window.location.reload();
+  }
+});
